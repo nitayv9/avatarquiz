d65404ff686e17c7c235f3c143b398f6ddb089e3
nitayv9 <71130268+nitayv9@users.noreply.github.com>
2023-01-16 15:58:06 +0200

    Update levinstein.py

--- levinstein.py
+++ levinstein.py
@@ -1,21 +1,20 @@
-"""
-The code of this fynction is taken from an answer in Stackoverflow by 'Salvador Deli'.
-Link to the question - https://stackoverflow.com/questions/2460177/edit-distance-in-python
-Link to Salvador Dali page - https://stackoverflow.com/users/1090562/salvador-dali
-"""
-
 
 def levenshteinDistance(s1, s2):
-    if len(s1) > len(s2):
-        s1, s2 = s2, s1
+    x = {}
+    fill_memo_table(len(s1) - 1, len(s2) - 1, s1, s2, x)
+    return x[(len(s1) - 1, len(s2) - 1)]
 
-    distances = range(len(s1) + 1)
-    for i2, c2 in enumerate(s2):
-        distances_ = [i2+1]
-        for i1, c1 in enumerate(s1):
-            if c1 == c2:
-                distances_.append(distances[i1])
-            else:
-                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
-        distances = distances_
-    return distances[-1]
+
+def fill_memo_table(i, j, s1, s2, x):
+    if x.get((i, j), -1) == -1:
+        if i == 0:
+            x[(i, j)] = j
+        elif j == 0:
+            x[(i, j)] = i
+        elif s1[i] == s2[j]:
+            x[(i, j)] = fill_memo_table(i-1, j-1, s1, s2, x)
+        else:
+            x[(i, j)] = 1 + min(fill_memo_table(i-1, j-1, s1, s2, x),
+                                fill_memo_table(i, j-1, s1, s2, x),
+                                fill_memo_table(i-1, j, s1, s2, x))
+    return x[(i, j)]
