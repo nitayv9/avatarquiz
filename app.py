8b7b8a231a0c50207145cb00d672006759f83935
nitayv9 <71130268+nitayv9@users.noreply.github.com>
2023-01-16 18:08:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -184,16 +184,5 @@ def credit():
     return render_template('credit.j2')
 
 
-def upload_questions_from_csv():
-    with open('avatar_questions.csv') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for line in csv_reader:
-            answers = line['answers'].split(',')
-            print(line['questions'])
-            insert_question(line['questions'], line['score'], line['pic'], *answers)
-def reset_db():
-    db.drop_all()
-    db.create_all()
-    upload_questions_from_csv()
 if __name__ == '__main__':
     app.run()
